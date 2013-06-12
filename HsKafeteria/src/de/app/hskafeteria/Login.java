52b99cde6681c8076a2f3be50925a9384b33e649
Adrian Müller <muad1011@hs-karlsruhe.de>
2013-06-12 12:15:44 +0200

    Bugbehebungen.

--- HsKafeteria/src/de/app/hskafeteria/Login.java
+++ HsKafeteria/src/de/app/hskafeteria/Login.java
@@ -216,21 +216,16 @@ public class Login extends FragmentActivity {
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-//	    switch (item.getItemId()) {
-//	        case android.R.id.home:
-//	            // app icon in action bar clicked; go home
-//	            Intent intent = new Intent(this, MainActivity.class);
-//	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//	            startActivity(intent);
-//	            return true;
-	            if (item.equals(settings.getItemId())) {
-	                showFragment(SETTINGS, true);
-	                return true;
-	            }
-	            return false;
-//	        default:
-//	            return super.onOptionsItemSelected(item);
-//	    }
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	            // app icon in action bar clicked; go home
+	            Intent intent = new Intent(this, MainActivity.class);
+	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	            startActivity(intent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 	protected void anmelden() {
